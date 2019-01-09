5d9dfb4eb1d36ddda2b8bfe8312e7c0581965fa1
Malcolm Rabson <malcolm.rabson@headoffice.dariel-solutions>
2019-01-09 16:13:34 +0200

    Added navbar

--- public/modules/modules.js
+++ public/modules/modules.js
@@ -1,10 +1,36 @@
 var ui = {};
 
 ui.navigation = `
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <ul class="nav navbar-nav">
-                <li><a href="#">Login</a></li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Create Account</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Deposit</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Withdraw</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Transactions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Balance</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">All Data</a>
+                </li>
             </ul>
         </div>
     </nav>
@@ -43,41 +69,41 @@ ui.allData = `
     <!-- ------------- YOUR CODE: All Data UI ------------- --> 
 `;
 
-var target     = document.getElementById('target');
+var target = document.getElementById('target');
 var navigation = document.getElementById('navigation');
 navigation.innerHTML += ui.navigation;
 
 
 
-var loadCreateAccount = function(){
+var loadCreateAccount = function () {
     target.innerHTML = ui.createAccount;
 };
 
-var loadLogin = function(){
+var loadLogin = function () {
     target.innerHTML = ui.login;
 };
 
-var loadDeposit = function(){
+var loadDeposit = function () {
     target.innerHTML = ui.deposit;
 };
 
-var loadWithdraw = function(){
+var loadWithdraw = function () {
     target.innerHTML = ui.withdraw;
 };
 
-var loadTransactions = function(){
+var loadTransactions = function () {
     target.innerHTML = ui.transactions;
 };
 
-var loadBalance = function(){
+var loadBalance = function () {
     target.innerHTML = ui.balance;
 };
 
-var defaultModule = function(){
+var defaultModule = function () {
     target.innerHTML = ui.default;
 };
 
-var loadAllData = function(){
+var loadAllData = function () {
     target.innerHTML = ui.allData;
 };
 
